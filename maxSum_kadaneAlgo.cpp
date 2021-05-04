231fa8e74bc1eba4bd97a922169c8558da2adee7
aashutosh213 <aashutosh213@gmail.com>
2021-05-04 22:46:19 +0530

    Make program for maximum sum of contagious sub array using kadane's alogrithm (most efficient): O(n)

---
+++ maxSum_kadaneAlgo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+int maxSum(const int* const a,int n){
+    int ans {0}, sum{0};
+    for(int i{0}; i<n; i++)
+    {
+
+        if(sum + a[i] > 0)
+            sum+=a[i];
+        else
+            sum = 0;
+        
+        ans = max(ans, sum);
+        // cout<< ans << "   ,sum:" <<sum<< endl;
+    }
+    return ans;
+}
+
+int main(){
+    int arr[]{2,-3,5,-9,7,4,1,-4};
+    int n = sizeof(arr)/sizeof(int);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i]<< ",";
+    }
+    cout <<endl;
+
+    cout << maxSum(arr,n) << endl;
+    
+    return 0;
+}
